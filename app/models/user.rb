b5c3611df0082d0686e4ecc2218456cc02808021
Shunjiro Yatsuzuka <syatsuzuka@gmail.com>
2022-05-24 22:52:40 +0900

    add avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :tutor_users, :class_name => "Course", :foreign_key => :tutor_user_id
   has_many :student_users, :class_name => "Course", :foreign_key => :student_user_team_id
+  has_one_attached :photo
 
   def select_label
     "#{first_name} #{last_name} <#{email}>"
