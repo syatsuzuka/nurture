86ccec394df8c75b71ba2bc80a280809efc72c35
Shunjiro Yatsuzuka <syatsuzuka@gmail.com>
2022-05-24 14:52:37 +0900

    add table in index

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -11,6 +11,7 @@ class CoursesController < ApplicationController
 
   def new
     @course = Course.new
+    authorize @course
   end
 
   def create
--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -1,36 +1,35 @@
 class CoursePolicy < ApplicationPolicy
   class Scope < Scope
-
     def resolve
       scope.all
     end
+  end
 
-    def index?
-      return true
-    end
+  def index?
+    true
+  end
 
-    def show?
-      true
-    end
+  def show?
+    true
+  end
 
-    def create?
-      true
-    end
+  def create?
+    true
+  end
 
-    def new?
-      create?
-    end
+  def new?
+    create?
+  end
 
-    def update?
-      true
-    end
+  def update?
+    true
+  end
 
-    def edit?
-      update?
-    end
+  def edit?
+    update?
+  end
 
-    def destroy?
-      true
-    end
+  def destroy?
+    true
   end
 end
