d9f4caf223b467b46e5595b476b9e6dc03b91262
Shunjiro Yatsuzuka <syatsuzuka@gmail.com>
2022-05-25 23:33:52 +0900

    add delete features

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :courses do
-
-    resources :assignments, only: %i[index new show create edit update]
+    resources :assignments
   end
 end
