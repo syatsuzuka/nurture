0f1391f5e697513d4c67e0068433802579807f85
Shunjiro Yatsuzuka <syatsuzuka@gmail.com>
2022-05-26 10:56:14 +0900

    add rails_admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_25_074334) do
+ActiveRecord::Schema.define(version: 2022_05_26_013444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 2022_05_25_074334) do
     t.string "last_name"
     t.string "avatar_url"
     t.string "role"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
