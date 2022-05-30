1dda0e88a515cdb8d84e083bf35cb7848e40ef14
James Reed <jamesreed.3050@gmail.com>
2022-05-30 15:54:19 +0900

    Move message view code to helper method file

--- app/helpers/chat_helper.rb
+++ app/helpers/chat_helper.rb
@@ -11,4 +11,13 @@ module ChatHelper
   def youtube_url_clean(data)
     return data.match(URI.regexp).to_s
   end
+
+  def text_or_youtube(message)
+    if youtube_link?(message.content)
+      YouTubeAddy.youtube_embed_url(youtube_url_clean(message.content),420,315).html_safe
+    else
+      message.content
+    end
+  end
+
 end
