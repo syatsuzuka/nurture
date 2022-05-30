e362834294a2f54f2acbed65ac40ec2f005f8dd1
Shunjiro Yatsuzuka <syatsuzuka@gmail.com>
2022-05-30 12:35:34 +0900

    fix validation in target and progress

--- app/models/progress.rb
+++ app/models/progress.rb
@@ -1,3 +1,13 @@
 class Progress < ApplicationRecord
   belongs_to :target
+  validate :check_date
+  validate :check_score
+
+  def check_date
+    errors.add(:date, "is required") if date.blank?
+  end
+
+  def check_score
+    errors.add(:score, "is required") if score.blank?
+  end
 end
