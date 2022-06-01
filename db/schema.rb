1be7b3f7adb54d71f71c7dd42cf3052a41e4007c
Edmund Oh <eddieoh0@gmail.com>
2022-06-01 11:58:32 +0900

    changed landing page

--- db/schema.rb
+++ db/schema.rb
@@ -186,8 +186,8 @@ ActiveRecord::Schema.define(version: 2022_05_30_075536) do
     t.string "last_name"
     t.string "avatar_url"
     t.string "role"
-    t.string "nickname"
     t.boolean "admin"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
