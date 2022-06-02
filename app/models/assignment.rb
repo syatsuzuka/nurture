a07b01260d6def8df97f9dffd59df5ad341e12a1
Shunjiro Yatsuzuka <syatsuzuka@gmail.com>
2022-06-02 23:23:26 +0900

    add_validation

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,12 +1,9 @@
 class Assignment < ApplicationRecord
   belongs_to :course
-  validates :title, uniqueness: { scope: :course_id }
-  validates :instruction, :checkpoint, presence: true
-  validate :check_status
+  validates :title, uniqueness: { scope: :course_id }, presence: true
+  validates :instruction, :checkpoint, :status, :start_date, :end_date, presence: true
   validate :check_instruction_url
   validate :check_material_url
-  # validates :instruction_url, format: /\A#{URI::regexp(%w(http https))}\z/
-  # validates :material_url, format: /\A#{URI::regexp(%w(http https))}\z/
 
   def check_instruction_url
     unless instruction_url.blank?
@@ -18,13 +15,5 @@ class Assignment < ApplicationRecord
     unless material_url.blank?
       errors.add(:material_url, "need to be a valid URL") unless material_url =~ /\A#{URI::regexp(%w(http https))}\z/
     end
-  end
-
-  def check_checkpoint
-    errors.add(:checkpoint, "can't be blank") if checkpoint.blank?
-  end
-
-  def check_status
-    errors.add(:status, "can't be blank") if status.blank?
   end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -3,8 +3,7 @@ class Course < ApplicationRecord
   belongs_to :student, :class_name => "User", :foreign_key => :student_user_id
   has_many :assignments, dependent: :destroy
   has_many :targets, dependent: :destroy
-  validate :check_name
-  validate :check_description
+  validates :name, :description, presence: true
   has_one_attached :photo
 
   include PgSearch::Model
@@ -13,12 +12,4 @@ class Course < ApplicationRecord
     using: {
       tsearch: { prefix: true }
     }
-
-  def check_name
-    errors.add(:name, "is required") if name.blank?
-  end
-
-  def check_description
-    errors.add(:description, "is required") if description.blank?
-  end
 end
