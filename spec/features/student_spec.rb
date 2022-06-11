d9fbdf3b13b27e81dbfe8a60dd2700ec6344ccb3
Shunjiro Yatsuzuka <syatsuzuka@gmail.com>
2022-06-11 16:25:14 +0900

    resolve rubocop issue

--- spec/features/student_spec.rb
+++ spec/features/student_spec.rb
@@ -8,12 +8,4 @@ feature 'student' do
     click_on('Students')
     expect(page).to have_content('Student List')
   end
-
-  scenario 'accesses to tutor menu' do
-    new_session_options = { user_email: ENV['STUDENT_LOGIN_ID'], user_password: ENV['STUDENT_LOGIN_PASSWORD'] }
-    new_session_form = NewSessionForm.new
-    new_session_form.visit_page.fill_in_with( new_session_options ).submit
-    click_on('Tutors')
-    expect(page).to have_content('Tutor List')
-  end
 end
