3afb709b3f6b6cf65be7c050da2f27fcc02384d0
Shunjiro Yatsuzuka <syatsuzuka@gmail.com>
2022-06-12 09:40:45 +0900

    switch to highcharts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "controllers"
+import "bootstrap"
+// import "chartkick/chart.js"
+import "chartkick/highcharts"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-import "controllers"
-import "bootstrap"
-import "chartkick/chart.js"
