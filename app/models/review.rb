ff0d745498db68bce9aa2fc44d424a449abe54f0
Shunjiro Yatsuzuka <syatsuzuka@gmail.com>
2022-06-14 19:50:01 +0900

    fix review default value

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   belongs_to :tutor, class_name: "User"
   belongs_to :student, class_name: "User"
   validates :tutor, uniqueness: { scope: :student }
+  validates :stars, presence: true
 end
