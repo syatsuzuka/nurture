d749967a7e34f4258934ab731037f0ce900d8287
Shunjiro Yatsuzuka <syatsuzuka@gmail.com>
2022-06-16 15:42:26 +0900

    delete avatar_url from user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_14_071812) do
+ActiveRecord::Schema.define(version: 2022_06_16_063928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -188,7 +188,6 @@ ActiveRecord::Schema.define(version: 2022_06_14_071812) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "avatar_url"
     t.string "role"
     t.string "nickname"
     t.boolean "admin"
