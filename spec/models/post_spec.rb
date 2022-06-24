04886426c8890b24e4346db8e8c76c4efe4440cc
James Reed <jamesreed.3050@gmail.com>
2022-06-24 11:21:43 +0900

    created validation specs to test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes"
+  it "is not valid without a title"
+  it "is not valid without a content"
+  it "is not valid without a user"
+  it "is not valid without a start_date"
+  it "is not valid without a start_date"
+
 end
