3ed854c49bce23c05e026396328491306e7bbf90
James Reed <jamesreed.3050@gmail.com>
2022-06-24 15:15:40 +0900

    Testing post title validation

---
+++ spec/factories/post.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+
+  factory :post do
+    association :user
+    title {Faker::Lorem.sentence(word_count: 5)}
+    content {Faker::Lorem.paragraph(sentence_count: 5)}
+  end
+
+end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
 
   it "is valid with valid attributes" do
-    expect(Post.new(user: (build :user))).to be_valid
+    expect(build :post).to be_valid
   end
 
-  it "is not valid without a title"
+  it "is not valid without a title" do
+    expect(build :post, title: nil).to_not be_valid
+  end
   it "is not valid without a content"
   it "is not valid without a user"
   it "is not valid without a start_date"
