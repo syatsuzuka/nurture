464f448ed6bd03534853b52a33c34fdc6fa26598
James Reed <jamesreed.3050@gmail.com>
2022-06-24 16:08:58 +0900

    Linting

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,12 +1,10 @@
 FactoryBot.define do
-
   factory :user do
-    email {"test@gmail.com"}
-    password {"blahblah123"}
-    first_name {"Taro"}
-    last_name {"Doe"}
-    nickname {"Taradoe"}
-    role {"tutor"}
+    email { "test@gmail.com" }
+    password { "blahblah123" }
+    first_name { "Taro" }
+    last_name { "Doe" }
+    nickname { "Taradoe" }
+    role { "tutor" }
   end
-
 end
