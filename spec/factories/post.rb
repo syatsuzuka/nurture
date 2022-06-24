8dc2f3c016336f4b9309334107eb2bb03a870563
James Reed <jamesreed.3050@gmail.com>
2022-06-24 16:11:02 +0900

    Linting post spec

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
 
   factory :post do
     association :user
-    title {Faker::Lorem.sentence(word_count: 5)}
-    content {Faker::Lorem.paragraph(sentence_count: 5)}
+    title { Faker::Lorem.sentence(word_count: 5) }
+    content { Faker::Lorem.paragraph(sentence_count: 5) }
   end
 
 end
