5557bbd0c520beded4b10a0b0f475ed59779a732
Shunjiro Yatsuzuka <syatsuzuka@gmail.com>
2022-06-24 21:07:58 +0900

    add feature test scenario

--- spec/features/progress_spec.rb
+++ spec/features/progress_spec.rb
@@ -11,19 +11,69 @@ feature 'progress' do
 
     #======= Access to Target detail =======
     click_on('Courses')
-    click_on('Tennis Lesson (Intermediate)')
-    click_on('Return Ace Rate (%)')
-    expect(page).to have_content('Return Ace Rate (%)')
+    click_on('Tennis Lesson (Beginner)')
+    click_on('Backhand Stroke (%)')
+    expect(page).to have_content('Backhand Stroke (%)')
 
     #======= Create a new Progress =======
     find('#add-progress').click
     new_progress_form = NewProgressForm.new
     progress_params = {
       progress_date: "2022-06-01",
-      progress_scope: 100,
+      progress_score: 100,
       progress_comment: "test_comment"
     }
     new_progress_form.fill_in_with(progress_params).submit
     expect(page).to have_content('test_comment')
+  end
+
+  scenario 'edits an existing progress' do
+    #======= Login with Tutor ID =======
+    new_session_form = NewSessionForm.new
+    session_params = { user_email: ENV['DEMO_TUTOR_LOGIN_ID'], user_password: ENV['DEMO_TUTOR_LOGIN_PASSWORD'] }
+    new_session_form.visit_page.fill_in_with(session_params).submit
+
+    #======= Access to Target detail =======
+    click_on('Courses')
+    click_on('Tennis Lesson (Beginner)')
+    click_on('Backhand Stroke (%)')
+    expect(page).to have_content('Backhand Stroke (%)')
+
+    #======= Create a new Progress =======
+    find('#progresses > table > tbody > tr:nth-child(1) > td > a.edit-progress').click
+    new_progress_form = NewProgressForm.new
+    progress_params = {
+      progress_date: "2022-06-01",
+      progress_score: 100,
+      progress_comment: "test_comment"
+    }
+    new_progress_form.fill_in_with(progress_params).submit
+    expect(page).to have_content('2022-06-01')
+    expect(page).to have_content('100')
+    expect(page).to have_content('test_comment')
+    expect(page).to have_no_content('2020-04-01')
+  end
+
+  scenario 'uploades new progresses' do
+    #======= Login with Tutor ID =======
+    new_session_form = NewSessionForm.new
+    session_params = { user_email: ENV['DEMO_TUTOR_LOGIN_ID'], user_password: ENV['DEMO_TUTOR_LOGIN_PASSWORD'] }
+    new_session_form.visit_page.fill_in_with(session_params).submit
+
+    #======= Access to Target detail =======
+    click_on('Courses')
+    click_on('Tennis Lesson (Beginner)')
+    click_on('Backhand Stroke (%)')
+    expect(page).to have_content('Backhand Stroke (%)')
+
+    #======= Create a new Progress =======
+    find('#upload-progress').click
+    attach_file('file', 'public/sample/nurture_progress_sample.csv')
+    click_on('Import')
+    expect(page).to have_content('2022-06-01')
+    expect(page).to have_content('2022-06-01')
+    expect(page).to have_content('2022-06-01')
+    expect(page).to have_content('100')
+    expect(page).to have_content('test')
   end
 end
--- spec/support/new_assignment_form.rb
+++ spec/support/new_assignment_form.rb
@@ -11,6 +11,20 @@ class NewAssignmentForm
     self
   end
 
+  def fill_in_with2(params = {})
+    fill_in('assignment_title', with: params.fetch(:assignment_title, "test_title"))
+    fill_in('assignment_instruction', with: params.fetch(:assignment_instruction, "test_instruction"))
+    fill_in('assignment_instruction_url', with: params.fetch(:assignment_instruction_url, "https://www.nurture.pw"))
+    fill_in('assignment_checkpoint', with: params.fetch(:assignment_checkpoint, "test_checkpoint"))
+    select(params.fetch(:assignment_status, "Work In Process"), from: 'assignment_status')
+    fill_in('assignment_comment', with: params.fetch(:assignment_comment, "test_comment"))
+    fill_in('assignment_material_url', with: params.fetch(:assignment_material_url, "https://www.nurture.pw"))
+    fill_in('assignment_review_comment', with: params.fetch(:assignment_review_comment, "test_review_comment"))
+    find('#assignment_start_date').set(params.fetch(:assignment_start_date, "2022-01-01"))
+    find('#assignment_end_date').set(params.fetch(:assignment_end_date, "2022-12-31"))
+    self
+  end
+
   def submit
     click_button('Save')
     self
