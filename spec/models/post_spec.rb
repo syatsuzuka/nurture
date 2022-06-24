620215c935b813dbc3b4fed7def34451b3c1ac95
James Reed <jamesreed.3050@gmail.com>
2022-06-24 17:45:48 +0900

    Added post content length test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+    #  -----Present validations-----
   it "is valid with valid attributes" do
     expect(build(:post)).to be_valid
   end
@@ -16,7 +17,7 @@ RSpec.describe Post, type: :model do
   it "is not valid without a user" do
     expect(build(:post, user: nil)).to_not be_valid
   end
-
+  #  -----Length validations-----
   it "is not valid with a title over 70 characters" do
     expect(build(:post, title: 'x' * 71)).to_not be_valid
   end
@@ -24,4 +25,15 @@ RSpec.describe Post, type: :model do
   it "is not valid with a title less than 6 characters" do
     expect(build(:post, title: 'x' * 5)).to_not be_valid
   end
+
+  it "is not valid with a content less than 30 characters" do
+    expect(build(:post, title: 'x' * 28)).to_not be_valid
+  end
+
+  it "is not valid with a content over 3000 characters" do
+    expect(build(:post, title: 'x' * 3002)).to_not be_valid
+  end
+  #  -----Uniqueness validations-----
+
+
 end
