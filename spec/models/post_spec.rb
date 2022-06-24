af07d3bebd8a1080e566621d14a4a03b130d78b3
James Reed <jamesreed.3050@gmail.com>
2022-06-24 15:29:40 +0900

    added title presence validation

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,17 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-
   it "is valid with valid attributes" do
-    expect(build :post).to be_valid
+    expect(build(:post)).to be_valid
   end
 
   it "is not valid without a title" do
-    expect(build :post, title: nil).to_not be_valid
+    expect(build(:post, title: nil)).to_not be_valid
   end
+
   it "is not valid without a content"
   it "is not valid without a user"
   it "is not valid without a start_date"
   it "is not valid without a start_date"
-
 end
