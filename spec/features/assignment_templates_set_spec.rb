55cf4c1c09aa4c37caa1a32a47f85ed67e1dd158
Shunjiro Yatsuzuka <syatsuzuka@gmail.com>
2022-06-26 17:12:05 +0900

    add assignment_templates_set test

--- spec/features/assignment_templates_set_spec.rb
+++ spec/features/assignment_templates_set_spec.rb
@@ -22,5 +22,29 @@ feature 'assignment_templates_set' do
     }
     assignment_templates_set_form.visit_page.fill_in_with(assignment_templates_set_params).submit
     expect(page).to have_content('test_name')
+    expect(page).to have_content('test_category')
+  end
+
+  scenario 'edits an existing assignment_templates_set' do
+    #======= Login with Tutor ID =======
+    session_form = SessionForm.new
+    session_params = { user_email: ENV['DEMO_TUTOR_LOGIN_ID'], user_password: ENV['DEMO_TUTOR_LOGIN_PASSWORD'] }
+    session_form.visit_page.fill_in_with(session_params).submit
+
+    #======= Access to Template menu =======
+    click_on('Template')
+    expect(page).to have_content('Template')
+
+    #======= Create a new Assignment Templates Set =======
+    find("#assignment-templates-sets > tbody > tr:nth-child(1) > td > a.edit-assignment-templates-set").click
+    assignment_templates_set_form = AssignmentTemplatesSetForm.new
+    assignment_templates_set_params = {
+      assignment_templates_set_name: "test_name",
+      assignment_templates_set_category: "test_category",
+      assignment_templates_set_visible: true
+    }
+    assignment_templates_set_form.fill_in_with(assignment_templates_set_params).submit
+    expect(page).to have_content('test_name')
+    expect(page).to have_content('test_category')
   end
 end
