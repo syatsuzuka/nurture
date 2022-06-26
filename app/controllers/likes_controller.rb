589c104c6a9c883a0e188f7d376911c25cf411c4
James Reed <jamesreed.3050@gmail.com>
2022-06-26 21:52:47 +0900

    form for like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,14 @@ class LikesController < ApplicationController
 
   def create
     @like = current_user.likes.new(like_params)
-
+    authorize @like
     flash[:notice] = @like.errors.messages unless @like.save
 
     redirect_to @like.post
   end
 
   def destroy
+    authorize @like
     @like = current_user.likes.find(params[:id])
     @like.destroy
     redirect_to @like.post
