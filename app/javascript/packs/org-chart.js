1235550dd2548e0fec98e4fc5fbecd312600f541
Shunjiro Yatsuzuka <syatsuzuka@gmail.com>
2022-07-01 00:58:06 +0900

    add org chart

--- app/javascript/packs/org-chart.js
+++ app/javascript/packs/org-chart.js
@@ -5,7 +5,7 @@ Highcharts.chart('org-chart', {
   },
 
   title: {
-      text: 'Highcharts Org Chart'
+      text: 'Organization'
   },
 
   accessibility: {
@@ -24,86 +24,40 @@ Highcharts.chart('org-chart', {
       type: 'organization',
       name: 'Highsoft',
       keys: ['from', 'to'],
-      data: [
-          ['Shareholders', 'Board'],
-          ['Board', 'CEO'],
-          ['CEO', 'CTO'],
-          ['CEO', 'CPO'],
-          ['CEO', 'CSO'],
-          ['CEO', 'HR'],
-          ['CTO', 'Product'],
-          ['CTO', 'Web'],
-          ['CSO', 'Sales'],
-          ['HR', 'Market'],
-          ['CSO', 'Market'],
-          ['HR', 'Market'],
-          ['CTO', 'Market']
-      ],
-      levels: [{
-          level: 0,
-          color: 'silver',
-          dataLabels: {
-              color: 'black'
-          },
-          height: 25
-      }, {
-          level: 1,
-          color: 'silver',
-          dataLabels: {
-              color: 'black'
-          },
-          height: 25
-      }, {
-          level: 2,
-          color: '#980104'
-      }, {
-          level: 4,
-          color: '#359154'
-      }],
-      nodes: [{
-          id: 'Shareholders'
-      }, {
-          id: 'Board'
-      }, {
-          id: 'CEO',
-          title: 'CEO',
-          name: 'Grethe Hjetland',
-          image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131126/Highsoft_03862_.jpg'
-      }, {
-          id: 'HR',
-          title: 'HR/CFO',
-          name: 'Anne Jorunn Fjærestad',
-          color: '#007ad0',
-          image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131210/Highsoft_04045_.jpg'
-      }, {
-          id: 'CTO',
-          title: 'CTO',
-          name: 'Christer Vasseng',
-          image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131120/Highsoft_04074_.jpg'
-      }, {
-          id: 'CPO',
-          title: 'CPO',
-          name: 'Torstein Hønsi',
-          image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131213/Highsoft_03998_.jpg'
-      }, {
-          id: 'CSO',
-          title: 'CSO',
-          name: 'Anita Nesse',
-          image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2020/03/17131156/Highsoft_03834_.jpg'
-      }, {
-          id: 'Product',
-          name: 'Product developers'
-      }, {
-          id: 'Web',
-          name: 'Web devs, sys admin'
-      }, {
-          id: 'Sales',
-          name: 'Sales team'
-      }, {
-          id: 'Market',
-          name: 'Marketing team',
-          column: 5
-      }],
+      data: gon.org_data,
+      // data: [
+      //     ['test', 'james'],
+      //     ['james', 'ed'],
+      //     ['james', 'shunjiro'],
+      //     ['ed', 'shingo'],
+      //     ['shunjiro', 'doug']
+      // ],
+      nodes: gon.org_nodes,
+    //   nodes: [{
+    //     id: 'test',
+    //     title: 'test',
+    //     name: 'James Reed'
+    // }, {
+    //       id: 'james',
+    //       title: 'james',
+    //       name: 'James Reed'
+    //   }, {
+    //       id: 'ed',
+    //       title: 'ed',
+    //       name: 'Ed Oz'
+    //   }, {
+    //       id: 'shunjiro',
+    //       title: 'shunjiro',
+    //       name: 'Shunjiro Yatsuzuka'
+    //   }, {
+    //       id: 'shingo',
+    //       title: 'shingo',
+    //       name: 'Shingo Kubomura'
+    //   }, {
+    //       id: 'doug',
+    //       title: 'doug',
+    //       name: 'Doug Berkeley'
+    //   }],
       colorByPoint: false,
       color: '#007ad0',
       dataLabels: {
