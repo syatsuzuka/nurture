1407439b2c975b3694d9931b5aaf131c9b980551
James Reed <jamesreed.3050@gmail.com>
2022-07-16 22:13:02 +0900

    Removed old comment from chat partial

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,13 +7,14 @@ class MessagesController < ApplicationController
     @message.chatroom = @chatroom
     @message.user = current_user
     authorize @message
+
     if @message.save
       ChatroomChannel.broadcast_to(
         @chatroom,
         render_to_string(partial: "message", locals: { message: @message })
       )
-      # redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
       head :ok
+      raise
     else
       render "chatrooms/show"
     end
