87965eec093c57b816c8acccc13940e3907fe2d8
James Reed <jamesreed.3050@gmail.com>
2022-07-22 10:12:18 +0900

    correcting migration file name

--- db/migrate/20220719132622_add_read_to_messages.rb
+++ db/migrate/20220719132622_add_read_to_messages.rb
@@ -1,4 +1,4 @@
-class AddViewedAtToMessages < ActiveRecord::Migration[6.1]
+class AddReadToMessages < ActiveRecord::Migration[6.1]
   def change
     add_column :messages, :read, :datetime, null: true
   end
