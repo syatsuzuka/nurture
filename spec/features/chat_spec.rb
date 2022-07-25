8a3310200fb1fd719c5f01df3272d1f5ad6b6f4a
Shunjiro Yatsuzuka <syatsuzuka@gmail.com>
2022-07-25 13:41:34 +0900

    add chat feature test

---
+++ spec/features/chat_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+require_relative '../support/session_form'
+require_relative '../support/assignment_form'
+require_relative '../support/chat_form'
+
+feature 'chat' do
+  scenario 'creates a new message' do
+    #======= Login with Tutor ID =======
+    session_form = SessionForm.new
+    session_params = {
+      user_email: ENV.fetch('DEMO_TUTOR_LOGIN_ID'),
+      user_password: ENV.fetch('DEMO_TUTOR_LOGIN_PASSWORD')
+    }
+    session_form.visit_page.fill_in_with(session_params).submit
+
+    #======= Access to course detail =======
+    click_on('courses-menu')
+    click_on('Tennis Lesson (Beginner)')
+    expect(page).to have_content('Tennis Lesson (Beginner)')
+
+    #======= Send message in chat =======
+    find('#chat-toggle').click
+    expect(page).to have_selector('input.send')
+    chat_form = ChatForm.new
+    chat_params = {
+      message_content: "test_message"
+    }
+    chat_form.fill_in_with(chat_params).submit
+
+    #======= Re-access to chat with Student account =======
+    # the test can't be passed unless we reopen the modal page
+    session_form.visit_page.logout
+    session_params = {
+      user_email: ENV.fetch('DEMO_STUDENT_LOGIN_ID'),
+      user_password: ENV.fetch('DEMO_STUDENT_LOGIN_PASSWORD')
+    }
+    session_form.visit_page.fill_in_with(session_params).submit.visit_page
+    click_on('courses-menu')
+    click_on('Tennis Lesson (Beginner)')
+    find('#chat-toggle').click
+    expect(page).to have_text('test_message')
+    chat_params = {
+      message_content: "https://www.youtube.com/watch?v=vUCB7LS5Cw8"
+    }
+    chat_form.fill_in_with(chat_params).submit
+
+    #======= Re-access to chat with Tutor account =======
+    session_form.visit_page.logout
+    session_params = {
+      user_email: ENV.fetch('DEMO_TUTOR_LOGIN_ID'),
+      user_password: ENV.fetch('DEMO_TUTOR_LOGIN_PASSWORD')
+    }
+    session_form.visit_page.fill_in_with(session_params).submit.visit_page
+    click_on('courses-menu')
+    click_on('Tennis Lesson (Beginner)')
+    find('#chat-toggle').click
+    expect(page).to have_text('test_message')
+  end
+end
