a30effacb8edd647abde4ed628bef2fa0f74435e
James Reed <jamesreed.3050@gmail.com>
2022-08-11 13:56:53 +0900

    updated controller to add view flagging of messages

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -4,5 +4,12 @@ class ChatroomsController < ApplicationController
     @chatroom = Chatroom.find(params[:id])
     authorize @chatroom
     @message = Message.new
+    messages = @chatroom.messages
+    new_messages = messages.where.not(user_id: current_user[:id]).where(read: nil)
+    unless new_messages.empty?
+      new_messages.each do |message|
+        message.update_attribute(:read, DateTime.now)
+      end
+    end
   end
 end
